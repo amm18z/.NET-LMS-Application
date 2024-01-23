1b89765af967d0e0f668b5e6004b05db9cb08e6c
amm18z <amm18z@my.fsu.edu>
2024-01-23 02:34:46 -0500

    Recorded Lecture 1 Notes
    I followed a long, wrote some learning code, and made comments to help myself.

--- Canvas/Canvas/Program.cs
+++ Canvas/Canvas/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace Canvas
+namespace Canvas //this is a namespace (logical), it has a corresponding assembly (physical) somewhere probably called "Canvas.DLL". I think?
 {
-    internal class Program //this is class definition/declaration. 'internal' access modifier for class 
+    internal class Program //this is class definition/declaration. 'internal' access modifier for class means this class can be accessed by any code in the same assembly
     {
-        static void Main(string[] args) //main function has specified signature, which is how application knows where to pick up the program/where the entry point is (identical to java)
+        private string myString; // example of a "field"
+
+        public static string MyString // example of a "property" with default getter and setter
         {
+            get; set;
+        }
+        static void Main(string[] args) //this main function has specified signature, which is how application knows where to pick up the program/where the entry point is (identical to java)
+        {
+            List<int> ints = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9 }; // list object example
+
+            foreach(var i in ints) //enumerated for loop example AND var example
+            {
+                Console.WriteLine(i);
+            }
+
             Console.WriteLine("Hello World!");
+
+            MyString = "Some Value"; // implicitly calling the setter (infix)
         }
     }
 }
