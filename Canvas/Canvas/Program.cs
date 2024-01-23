a70425712a5fc8b2cbd6108893a081d1377425a8
amm18z <amm18z@my.fsu.edu>
2024-01-22 23:06:53 -0500

    Pressed "Create .NET Project" in VSCode
    From desktop

---
+++ Canvas/Canvas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Canvas
+{
+    internal class Program //this is class definition/declaration. 'internal' access modifier for class 
+    {
+        static void Main(string[] args) //main function has specified signature, which is how application knows where to pick up the program/where the entry point is (identical to java)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
